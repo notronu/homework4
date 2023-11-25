c5ecbce3f6e5aab39e3d5f7b616f6d41c9da11f7
Chika_Pyu <ya.sasha-mochalov1@yandex.ru>
2023-11-25 13:39:27 +0300

    com2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задача 1");
-        int age = 22;
+        int age = 21;
         if (age >= 18) {
             System.out.println("Если возраст человека равен " + age + " ,то он совершеннолетний");
         }
